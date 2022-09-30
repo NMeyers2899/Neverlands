32d9c0ef4cd7d26ab191490850da78e6ea73f7ee
Nicholas <nmeyers2899@gmail.com>
2022-09-30 12:47:06 -0500

    Created a Way to Create New Unit Types

--- Assets/Scripts/Gameplay/UnitStats.cs
+++ Assets/Scripts/Gameplay/UnitStats.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Unit Type", menuName = "Unit/Create New Unit Type")]
 public class UnitStats : ScriptableObject
 {
     /// <summary>
